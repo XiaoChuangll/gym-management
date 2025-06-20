392004b3a77bb8126cc8a53a874825a45e0864c7
XiaoChuangll <2983995678@qq.com>
2025-06-20 16:36:01 +0800

    添加会员预约图标和菜单项

--- src/main/java/com/gym/management/GymManagementApplication.java
+++ src/main/java/com/gym/management/GymManagementApplication.java
@@ -11,7 +11,13 @@ public class GymManagementApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GymManagementApplication.class, args);
+        System.out.println("------------------------------------------------");
+        System.out.println(" ");
+        System.out.println("          服务已运行在：8080 端口");
+        System.out.println("          点击下面URL进行访问");
+        System.out.println("          http://localhost:8080/");
+        System.out.println(" ");
+        System.out.println("------------------------------------------------");
 
-        System.out.println("http://localhost:8080/");
     }
 } 
